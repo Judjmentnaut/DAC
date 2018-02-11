2b9a206157a55fa186a301968bfce5f978f3dbd9
Judjmentnaut <30499122+Judjmentnaut@users.noreply.github.com>
2018-02-12 02:20:05 +0900

    Add files via upload

--- Versioni stabili/versione 1.6 hidden sakura/versione 1.6 hidden sakura.c
+++ Versioni stabili/versione 1.6 hidden sakura/versione 1.6 hidden sakura.c
@@ -5,26 +5,52 @@ void import(char v[]);
 
 void cifrazione(char v[], int cif1, int cif2);
 
-void menu();
+void insman(char v[]);
+
+int menu();
 
 void decifrazione(char v[], int cif1, int cif2);
 
 
 int main(){
-	int cif1, cif2;
+	int cif1, cif2, scelta;
 	char v[DIM];
 	
-	menu();
+	printf("\n Caesar double ASCII cypher\n Current version : v1.4 **********\n author : Judjmentnaut\n\n\n Inserire le due chiavi di cifratura.\n Una volta inserite inserire una frase di massimo 100 caratteri.\n I caratteri in eccesso verranno scartati.\n il file di output di default e' [output.txt.]\n Se la frase non raggiunge i 20 caratteri e' consigliabile l'uso degli spazi come carattere nullo.\n La frase verra' cifrata usando un doppio cifratore (pari e dispari) di cesare basato sul formato ASCII.\n");
 	printf("\n prima chiave di cifratura : ");
 	scanf("%d", &cif1);
 	printf("\n seconda chiave di cifratura : ");
 	scanf("%d", &cif2);
-	import(v);
-	cifrazione(v, cif1, cif2);
+	scelta=menu();
+	
+	switch(scelta){
+		case 1:{
+			import(v);
+			cifrazione(v, cif1, cif2);
+			break;
+		}
+		
+		case 2:{
+			insman(v);
+			cifrazione(v, cif1, cif2);
+			break;
+		}
+	}
+	
+//	import(v);
+//	cifrazione(v, cif1, cif2);
 	decifrazione(v, cif1, cif2);
 	printf("\n\nl'operazione e' finita\nil file output contiene ora la frase cifrata\nil file out_def contine una copia della frase inserita");
 }
 
+
+void insman(char v[]){
+	int i;
+	
+	for(i=0;i<DIM;i++){
+		scanf("%c", v[i]);
+	}
+}
 
 
 void cifrazione(char v[],int cif1,int cif2){
@@ -84,7 +110,11 @@ void decifrazione(char v[], int cif1, int cif2){
 
 
 
-void menu(){
-	printf("\n Caesar double ASCII cypher\n Current version : v1.4 **********\n author : Judjmentnaut\n\n\n Inserire le due chiavi di cifratura.\n Una volta inserite inserire una frase di massimo 100 caratteri.\n I caratteri in eccesso verranno scartati.\n il file di output di default e' [output.txt.]\n Se la frase non raggiunge i 20 caratteri e' consigliabile l'uso dell' invio come carattere nullo.\n La frase verra' cifrata usando un doppio cifratore (pari e dispari) di cesare basato sul formato ASCII.\n");
-	// menu placeholder, verrà cambiato con l'implementazione delle librerie di cifrazione personalizzate
+int menu(){
+	int s;
+	
+	printf("\n\n seleziona la modalita' di inserimento: \n\n\t 1- inserimento da file\n\n\t 2- inerimento manuale");
+	scanf("%d", s);
+	
+	return s;
 }
